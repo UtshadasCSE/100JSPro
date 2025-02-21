50ef128558439bd9df5f7bcd36ecdfb72d8f9332
UtshadasCSE <utshachandradas@gmail.com>
2025-02-21 20:17:52 +0600

    Updated

--- StopwatchApp/script.js
+++ StopwatchApp/script.js
@@ -1,7 +1,7 @@
-const timer = document.getElementById("timer");
-const startButton = document.getElementById("start");
-const stopButton = document.getElementById("stop");
-const resetButton = document.getElementById("reset");
+const timerEl = document.getElementById("timer");
+const startButtonEl = document.getElementById("start");
+const stopButtonEl = document.getElementById("stop");
+const resetButtonEl = document.getElementById("reset");
 
 let startTime = 0;
 let elapsedTime = 0;
@@ -9,17 +9,19 @@ let timerInterval;
 
 function startTimer() {
   startTime = Date.now() - elapsedTime;
+
   timerInterval = setInterval(() => {
     elapsedTime = Date.now() - startTime;
-    timer.textContent = formateTime(elapsedTime);
+    timerEl.textContent = formatTime(elapsedTime);
   }, 10);
-  startButton.disabled = true;
-  stopButton.disabled = false;
+
+  startButtonEl.disabled = true;
+  stopButtonEl.disabled = false;
 }
 
-function formateTime() {
+function formatTime(elapsedTime) {
   const milliseconds = Math.floor((elapsedTime % 1000) / 10);
-  const seconds = Math.floor((elapsedTime * 60) / 1000);
+  const seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
   const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
   const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
   return (
@@ -32,22 +34,21 @@ function formateTime() {
     (milliseconds > 9 ? milliseconds : "0" + milliseconds)
   );
 }
-
 function stopTimer() {
-  clearInterval(timerInterval); // Stops the timer from updating
-  startButton.disabled = false; // Enables the Start button
-  stopButton.disabled = true; // Disables the Stop button
+  clearInterval(timerInterval);
+  startButtonEl.disabled = false;
+  stopButtonEl.disabled = true;
 }
 function resetTimer() {
-  clearInterval(timerInterval); // Stops the interval
+  clearInterval(timerInterval);
 
-  elapsedTime = 0; // Resets elapsed time
-  timer.textContent = "00:00:00"; // Resets the timer display
+  elapsedTime = 0;
+  timerEl.textContent = "00:00:00";
 
-  startButton.disabled = false; // Enables Start button
-  stopButton.disabled = true; // Disables Stop button
+  startButtonEl.disabled = false;
+  stopButtonEl.disabled = true;
 }
 
-startButton.addEventListener("click", startTimer);
-stopButton.addEventListener("click", stopTimer);
-resetButton.addEventListener("click", resetTimer);
+startButtonEl.addEventListener("click", startTimer);
+stopButtonEl.addEventListener("click", stopTimer);
+resetButtonEl.addEventListener("click", resetTimer);
